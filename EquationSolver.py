f4cda9cd909f44e54bb84380151e2755a262f86f
zlynna <75937288+zlynna@users.noreply.github.com>
2022-03-22 10:01:24 +0800

    Update EquationSolver.py

--- EquationSolver.py
+++ EquationSolver.py
@@ -15,11 +15,11 @@ tf.set_random_seed(1234)
 def main():
     y_range = np.array((-np.pi, np.pi))
     x_range = np.array((-np.pi, np.pi))
-    t_range = np.array((0., 1.))
-    NX = 100
-    Ny = 100
+    t_range = np.array((0., 2.))
+    NX = 30
+    Ny = 30
     Nt = 10
-    N_bc = 100
+    N_bc = 30
 
     data = DataSet(x_range, y_range, t_range, NX, Ny, Nt, N_bc)
     # input data
@@ -47,8 +47,8 @@ def main():
     fneq_ini_pre = net_neq(x_ini_train, y_ini_train, t_ini_train) / 1e4
 
     bgk = data.bgk(fneq_pre, rou_pre, u_pre, v_pre, x_train, y_train, t_train)
-    bgk_bc = data.bgk_cond(fneq_bc_pre, rou_bc_pre, u_bc_pre, v_bc_pre, x_bc_train, y_bc_train, t_bc_train)
-    fneq_bc = data.fBC(fneq_bc_pre, rou_bc_pre, u_bc_pre, v_bc_pre, x_b, y_b, t_b)
+    bgk_bc = data.bgk(fneq_bc_pre, rou_bc_pre, u_bc_pre, v_bc_pre, x_bc_train, y_bc_train, t_bc_train)
+    fneq_bc = data.fBC(fneq_bc_pre, rou_train, u_train, v_train, x_b, y_b, t_b)
 
     Eq_res = data.Eq_res(fneq_pre, rou_pre, u_pre, v_pre, x_train, y_train, t_train)
 
@@ -62,13 +62,18 @@ def main():
            tf.reduce_mean(bgk) + \
            tf.reduce_mean(bgk_bc) + \
            tf.reduce_mean(fneq_bc) * 1e8
-    # tf.reduce_mean(bgk_bc) + \
+    # tf.reduce_mean(fneq_ini_pre - fneq_ini_train) + \
 
-    train_adam = tf.train.AdamOptimizer(learning_rate=5e-4).minimize(loss)
+    start_lr = 1e-3
+    learning_rate = tf.train.exponential_decay(start_lr, global_step=5e4, decay_rate=1-5e-3, decay_steps=500)
+    train_adam = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
     train_lbfgs = tf.contrib.opt.ScipyOptimizerInterface(loss,
                                                          method="L-BFGS-B",
-                                                         options={'maxiter': 70000,
-                                                                  'ftol': 1.0 * np.finfo(float).eps
+                                                         options={'maxiter': 50000,
+                                                                  'maxfun': 70000,
+                                                                  'maxcor': 100,
+                                                                  'maxls': 100,
+                                                                  'ftol': 10.0 * np.finfo(float).eps
                                                                   }
                                                          )
     sess = tf.Session()
@@ -82,8 +87,8 @@ def main():
 
     Model = Train(tf_dict)
     start_time = time.perf_counter()
-    #Model.ModelTrain(sess, loss, train_adam, train_lbfgs)
-    Model.LoadModel(sess)
+    Model.ModelTrain(sess, loss, train_adam, train_lbfgs)
+    #Model.LoadModel(sess)
     stop_time = time.perf_counter()
     print('Duration time is %.3f seconds' % (stop_time - start_time))
 
